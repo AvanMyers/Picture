bcdb734646bfd92f75befa836f408f8485fb01ff
AvanMyers <smail1341@gmail.com>
2024-07-16 12:52:31 +0300

    go

---
+++ Pictures/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pictures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfImages = 52;
+            int imagesInRow = 3;
+            int filledRows;
+            int restOfTheImagenes;
+
+            filledRows = numberOfImages / imagesInRow;
+            restOfTheImagenes = numberOfImages % imagesInRow;
+
+            Console.WriteLine($"Заполненных рядов {filledRows}, свободных карточек осталось {restOfTheImagenes}");
+            Console.ReadKey();
+        }
+    }
+}
